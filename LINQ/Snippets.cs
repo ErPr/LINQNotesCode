70387a23de4ee1339716b419ae0cbff7571e2571
Eric <wunderkammer123@gmail.com>
2018-04-19 17:12:15 -0400

    Added snippets

---
+++ LINQ/Snippets.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ
+{
+    class Snippets
+    {
+
+//        using System;
+
+//namespace Treehouse.CodeChallenges
+//    {
+//        class Program
+//        {
+//            delegate int MathOperation(int number);
+
+//            public static int Add(int number)
+//            {
+//                return number + number;
+//            }
+
+//            public static int Square(int number)
+//            {
+//                return number * number;
+//            }
+
+//            static void Main(string[] args)
+//            {
+//                MathOperation add = new MathOperation(Add);
+
+//                MathOperation square = new MathOperation(Square);
+//            }
+
+//        }
+    }
+
+
+
+
+}
+}
