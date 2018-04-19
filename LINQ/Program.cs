c2e907002543619d157119d0e0ef921771a5d927
Eric <wunderkammer123@gmail.com>
2018-04-19 16:30:26 -0400

    Initial functional programming example.

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -10,9 +10,33 @@ namespace LINQ
     {
         static void Main(string[] args)
         {
+            // Querying related
             Querying.Notes();
             Querying.BirdNotes();
             Console.ReadLine();
+        }
+    }
+
+    class FunctionalProgrammingProgram
+    {
+        delegate void SayGreeting(string name);  //defines method without giving it any functionality
+                                                 //like a blueprint or signature
+                                                 //describes what it will do, not how
+
+        public static void SayHello(string name)
+        {
+            Console.WriteLine(string.Format("Hello, {0}", name));
+        }
+
+        static void Main(string[] args)
+        {
+            SayGreeting sayGreeting = new SayGreeting(SayHello);
+
+            Console.WriteLine("What's your name? ");
+            string input = Console.ReadLine();
+            sayGreeting(input);
+
+            Console.ReadLine(); //used to wait before closing CLI
         }
     }
 }
